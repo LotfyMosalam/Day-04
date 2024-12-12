ccb74e36bf6dd7f16dd491185d623dcb63248142
Administrator <Administrator@DESKTOP-FIK5SPH>
2024-12-12 23:27:21 +0200

    Add project files.

---
+++ Assignment/Program.cs
@@ -0,0 +1,203 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region 1-   Write a program that prints an identity matrix using for loop,in other words takes a value n from the user and shows the identity table of size n * n.
+
+            //Console.Write("Enter the size of the identity matrix (n): ");
+            //if (int.TryParse(Console.ReadLine(), out int Number) && Number > 0)
+            //{
+            //    Console.WriteLine("identity table of size:");
+            //    for (int r = 0; r < Number; r++)
+            //    {
+            //        for (int c = 0; c < Number; c++)
+            //        {
+            //            if (r == c)
+            //                Console.Write("1");
+            //            else
+            //                Console.Write("0");
+            //        }
+            //        Console.WriteLine();
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid input");
+            //}
+
+            #endregion
+
+
+            #region 2-   Write a program in C# Sharp to find the sum of all elements of the array
+
+            //int[] array = { 80, 93, 85, 89, 97, 81 };
+            //int sum = 0;
+            //foreach (int num in array)
+            //{
+            //    sum += num;
+            //}
+            //Console.WriteLine($"The sum of all elements in the array is: {sum}");
+
+            #endregion
+
+
+            #region 3-   Write a program in C# Sharp to merge two arrays of the same size sorted in ascending order.
+
+            //int[] arr01 = { 10, 3, 6, 7, 8 };
+            //int[] arr02 = { 1, 4, 5, 9, 2 };
+            //int[] mergedArr03= new int[arr01.Length + arr02.Length];
+            //arr01.CopyTo(mergedArr03, 0); 
+            //arr02.CopyTo(mergedArr03, arr01.Length);
+            //Array.Sort(mergedArr03);
+            //Console.WriteLine("array :");
+            //foreach (int num in mergedArr03)
+            //{
+            //    Console.Write(num+" - ");
+            //}
+            #endregion
+
+
+            #region 4-   Write a program in C# Sharp to count the frequency of each element of an array.
+
+            //int[] array = { 5, 2, 5, 1, 7, 3, 2, 8, 7, 4, 5, 9, 1, 6, 9, 3 };
+            //Console.WriteLine("Frequency of each element:");
+            //for (int r = 0; r < array.Length; r++)
+            //{
+            //    int count = 1;
+            //    if (array[r] == -1)
+            //        continue;
+            //    for (int c = r + 1; c < array.Length; c++)
+            //    {
+            //        if (array[r] == array[c])
+            //        {
+            //            count++;
+            //            array[c] = -1;
+            //        }
+            //    }
+            //    Console.WriteLine($"Element {array[r]} appears {count} time(s)");
+            //}
+            #endregion
+
+
+            #region 5-   Write a program in C# Sharp to find maximum and minimum element in an array.
+
+            //int[] array = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //int max = array[0];
+            //int min = array[0];
+            //foreach (int num in array)
+            //{
+            //    if (num > max)
+            //    {
+            //        max = num;
+            //    }
+            //    if (num < min)
+            //    {
+            //        min = num;
+            //    }
+            //}
+            //Console.WriteLine($"The maximum of element is: {max}");
+            //Console.WriteLine($"The minimum of element is: {min}");
+
+            #endregion
+
+
+            #region 6-   Write a program in C# Sharp to find the second largest element in an array.
+
+            //int[] arr = { 30, 24, 59, 25, 16, 77 };
+            //Array.Sort(arr);
+            //int secondLargest = arr[arr.Length - 2];
+            //Console.WriteLine("The second largest element in an array is: " + secondLargest);
+
+            #endregion
+
+
+            #region 7-   Consider an Array of Integer values with size N, having values as    
+
+
+            //Console.WriteLine("Enter the array values separated by spaces:");
+            //string input = Console.ReadLine();
+
+            //int[] array = Array.ConvertAll(input.Split(' '), int.Parse);
+
+            //Dictionary<int, int> firstOccurrence = new Dictionary<int, int>();
+            //int maxDistance = 0;
+
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    if (firstOccurrence.ContainsKey(array[i]))
+            //    {
+            //        int distance = i - firstOccurrence[array[i]];
+            //        maxDistance = Math.Max(maxDistance, distance);
+            //    }
+            //    else
+            //    {
+            //        firstOccurrence[array[i]] = i;
+            //    }
+            //}
+
+            //Console.WriteLine("The longest distance between two equal cells is: "
+
+            #endregion
+
+
+            #region 8-   Given a list of space separated words, reverse the order of the words.
+
+            //Console.WriteLine("Enter a sentence with space-separated words:");
+            //string input = Console.ReadLine();
+
+            //string[] words = input.Split(' ');
+            //Array.Reverse(words);
+
+            //Console.WriteLine(string.Join(" ", words));
+
+            #endregion
+
+
+            #region 9-   Write a program to create two multidimensional arrays of same size.
+            //int[,] array01 = new int[2, 3];
+            //int[,] array02 = new int[2, 3];
+            //Console.WriteLine("Enter elements for the first array : ");
+            //for (int r = 0; r < 2; r++)
+            //{
+            //    for (int c = 0; c < 3; c++)
+            //    {
+            //        Console.Write($"Enter element the position ({r},{c}): ");
+            //        array01[r, c] = int.Parse(Console.ReadLine());
+            //    }
+            //}
+            //Array.Copy(array01, array02, array01.Length);
+            //Console.WriteLine("The second array is : ");
+            //for (int r = 0; r < 2; r++)
+            //{
+            //    for (int c = 0; c < 3; c++)
+            //    {
+            //        Console.Write(array02[r, c] + " ");
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
+
+
+            #region 10-  Write a Program to Print One Dimensional Array in Reverse Order.
+
+            //int[] arr = new int[5];
+            //Console.WriteLine("Please enter 5 numbers:");
+            //for (int r = 0; r < arr.Length; r++)
+            //{
+            //    arr[r] = int.Parse(Console.ReadLine());
+            //}
+            //Console.WriteLine("Dimensional Array in Reverse Order are :");
+            //for (int r = arr.Length - 1; r >= 0; r--)
+            //{
+            //    Console.Write(arr[r] + " ");
+            //}
+
+            #endregion
+
+
+        }
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,277 @@
+﻿namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Part 01 One D Array
+            //// Examble 01 :
+
+            //int[] numbers;
+            //numbers = new int[3];
+
+            //numbers[0] = 1;
+            //numbers[1] = 2;
+            //numbers[2] = 3;
+
+            //Console.WriteLine(numbers[0]);
+            //Console.WriteLine(numbers[1]);
+            //Console.WriteLine(numbers[2]); 
+
+
+            ////// Examble 02 :
+
+            //int[] numbers = new int[3];
+            //Console.WriteLine($"The Size of Array = {numbers.Length}, Numbers of Dimensions = {numbers.Rank}");
+            ////foreach (int i in numbers)
+            ////{
+            ////    Console.WriteLine(i);
+            ////}
+
+            //numbers[0] = 1;
+            //numbers[1] = 2;
+            //numbers[2] = 3;
+
+            //if (numbers.Length > 3)
+            //{
+            //    numbers[3] = 4; 
+            //}
+            //Console.WriteLine();
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //}
+
+            #endregion
+
+
+            #region Part 02 Two D Array - Rectangular
+
+            ///uint[,] Grades = new uint[3, 5];
+            ///
+            ///Console.WriteLine($"The Size of Array = {Grades.Length}, Numbers of Dimensions = {Grades.Rank}");
+            ///
+            ///for (int r = 0; r < Grades.GetLength(0); r++)
+            ///{
+            ///    Console.WriteLine($"Please Insert The Grades for Student Number : ({r+1})");
+            ///    for(int c = 0; c < Grades.GetLength(1);)
+            ///    {
+            ///        Console.Write($"The Grade for the Subject No ({c + 1}) = "); 
+            ///      
+            ///        c = uint.TryParse(Console.ReadLine(), out Grades[r, c]) && Grades[r, c] <= 100 ? ++c : c;
+            ///    }
+            ///    Console.WriteLine();
+            ///}
+            ///
+            ///Console.Clear();
+
+            ///for (int r = 0; r < Grades.GetLength(0); r++)
+            ///{
+            ///    Console.WriteLine($"These are grades for student No : {r + 1}");
+            ///    for (int c = 0; c < Grades.GetLength(1); c++)
+            ///    {
+            ///        Console.WriteLine($"This Grade for Subject No ({c + 1}) = {Grades[r, c]}");
+            ///    }
+            ///    Console.WriteLine();
+            ///}
+
+            ///foreach (int grade in Grades)
+            ///{
+            ///    Console.WriteLine(grade);
+            ///}
+
+            ///for (int i = 0; i < Grades.Length; i++)
+            ///{
+            ///    Console.WriteLine(Grades[i/5, i%5]);
+            ///}
+
+            ///Grades[0, 0] = 99;
+            ///Grades[0, 1] = 98;
+            ///Grades[0, 2] = 92;
+            ///Grades[0, 3] = 93;
+            ///Grades[0, 4] = 100;
+            ///
+            ///Grades[1, 0] = 99;
+            ///Grades[1, 1] = 98;
+            ///Grades[1, 2] = 92;
+            ///Grades[1, 3] = 93;
+            ///Grades[1, 4] = 100;
+            ///
+            ///Grades[2, 0] = 99;
+            ///Grades[2, 1] = 98;
+            ///Grades[2, 2] = 92;
+            ///Grades[2, 3] = 93;
+            ///Grades[2, 4] = 100;
+
+            #endregion
+
+
+            #region Part 03 Two D Array - Judged Array
+
+            //int[][] Marks = new int[3][];
+
+            //Marks[0] = new int[3] { 1, 2, 3 };
+            //Marks[1] = new int[2] { 4, 5 };
+            //Marks[2] = new int[1] { 6 };
+
+            //for (int i = 0; i < Marks.Length; i++)
+            //{
+            //    Console.Write("Marks[" + i + "] = ");
+            //    foreach (int value in Marks[i])
+            //    {
+            //        Console.Write(value + " ");
+            //    }
+            //    Console.WriteLine(); 
+            //}
+
+            #endregion
+
+
+            #region Part 04 Array Methods
+
+            #region 1.  Class Member Methods [Static Methods]
+
+            //// Class Member Methods [Static Methods]
+
+            //int[] Numbers = { 10, 2, 5, 6, 4, 3, 1, 5, 9, 8 };
+            //int[] Array01 = { 1, 2, 3 };
+            //int[] Array02 = new int[2];
+
+            ////Array.Copy(Array01, Array02, 2);
+            ////Array.Sort(Numbers);
+            ////Array.Clear(Numbers);
+            ////Array.ConstrainedCopy(Array01, 1, Array02, 0, 2);
+            ////Array02 = (int[])Array.CreateInstance(typeof(int), 10);
+            ////int[,] Array03 = (int[,])Array.CreateInstance(typeof(int), 2, 4);
+            ////int[,,] Array04 = (int[,,])Array.CreateInstance(typeof(int), 2, 4, 2);
+
+            ////int index01 = Array.IndexOf(Numbers, 5);
+            ////Console.WriteLine(index01);
+            ////int index02 = Array.LastIndexOf(Numbers, 5);
+            ////Console.WriteLine(index02);
+
+            ////foreach (int Number in Array04)
+            ////{
+            ////    Console.WriteLine(Number);
+            ////} 
+            #endregion
+
+
+            #region 2.  Object Member Methods [Non Static Methods]
+
+            //int[] Numbers = { 10, 2, 5, 6, 4, 3, 1, 5, 9, 8 };
+            //int[] Array01 = { 1, 2, 3 };
+            //int[] Array02 = new int[5];
+
+            ////Array01.CopyTo(Array02, 1);
+            ////Console.WriteLine(Array01.GetLength(0)); 
+            ////Console.WriteLine(Array01.GetValue(1));
+            //Array01.SetValue(100, 1);
+
+            //foreach (int number in Array01)
+            //{
+            //    Console.WriteLine(number);
+            //}
+
+            #endregion
+
+            #endregion
+
+
+            #region Part 05 Boxing, UnBoxing
+
+            /// Boxing
+            ///
+            ///object obj;
+            ///obj = new object();
+            ///obj = new string("Nada");
+            ///obj = 5;    //Boxing
+            ///obj = 5.5;  //Boxing
+            ///obj = true; //Boxing
+            ///obj = 'A';  //Boxing
+            ///Console.WriteLine($"obj = {obj}");
+
+            ///un Boxing
+            ///object obj;
+            ///obj = new object();
+            ///int Number;
+            ///Number = (int)obj;  //unBoxing (un safe casting)
+            ///Number = obj.GetType() == typeof(int) ? (int)obj : 0; //safe
+            ///Number = obj is int ? (int)obj : 0;  //syntax suger
+            ///Console.WriteLine(Number);
+
+            #endregion
+
+
+            #region Part 07 Nullable - Value Types
+
+            //Example 1
+
+            //Nullable<int> Age;
+            //int? Age; //syntax suger
+            //Age = null;
+            //Console.WriteLine(Age is null? 0: Age);
+            //Console.WriteLine(Age.HasValue? Age: 0);
+
+            //Example 2
+
+            //Nullable<decimal> salary;
+            //decimal? salary;
+            //salary = null;
+            //salary = 5_000;
+            //Console.WriteLine(salary is null? 0: salary);
+            //Console.WriteLine(salary.HasValue? salary: 0);
+
+            //Example 3
+
+            //int x = 10;             //x Allows int only
+            //int? y = x;          //y Allows int + null
+            //Console.WriteLine(y);
+            //casting from int [value type] to Nullable<int>[Nullable value type}
+            //safe castiing
+
+
+            #endregion
+
+
+            #region Part 08 Nullable - Reference Types  
+
+            //string  Message01=null!;  //! null forgiveness operator
+            //string? Message02=null;
+            //Console.WriteLine(Message01);
+            //Console.WriteLine(Message02);
+
+            #endregion
+
+
+            #region Part 09 Null Propagation Operator
+
+            //Example 1
+
+            //int[]? Numbers = { 1, 2, 3, 4, 5 };
+            //if (Numbers is not null)
+            //{
+            //    for (int i = 0; i < Numbers.Length; i++)
+            //    {
+            //        Console.WriteLine(Numbers[i]);
+            //    }
+            //}
+
+            //Example 2
+
+            //int[]? Numbers = { 1, 2, 3, 4, 5 };
+            //int length;
+            //if (Numbers is not null)
+            //    length = Numbers.Length;
+            //else
+            //    length = 0;
+
+            //length = Numbers?.Length ?? 0;
+
+            #endregion
+
+        }
+    }
+}
